d6a0467affaca7681ec1b28b19c22d8c7dfffced
BhargavBunty <bhargavbunty@059gmail.com>
2024-02-27 01:18:49 +0530

    add create_all_user_groups

--- complaint_registration/management/commands/create_user_groups.py
+++ complaint_registration/management/commands/create_user_groups.py
@@ -11,3 +11,17 @@ class Command(BaseCommand):
 
         student.permissions.add(modify_complaint_permission)
         student.permissions.add(create_complaint_permission)
+
+        hod = Group.objects.create(name='hod')
+
+        view_sensitive_complaint_permission = Permission.objects.get(codename='view_sensitive_complaint')
+        hod.permissions.add(view_sensitive_complaint_permission)
+
+        counsellor = Group.objects.create(name='counsellor')
+
+        modify_complaint_permission = Permission.objects.get(codename='modify_complaint')
+        create_complaint_permission = Permission.objects.get(codename='create_complaint')
+
+        counsellor.permissions.add(create_complaint_permission)
+        counsellor.permissions.add(modify_complaint_permission)
+        # Add other permissions
