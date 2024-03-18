6e5c56c0a0baeda8ac2742724e962177af7f8a87
BhargavBunty <bhargavbunty@059gmail.com>
2024-03-19 00:12:40 +0530

    Added Polymorphic model relations(generic) for users that register the complaint and user to which complaint has been assigned to

--- complaint_administration/models/complaint_models.py
+++ complaint_administration/models/complaint_models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from user_administration.models import EscalationStructure
+from user_administration.escalation_models import EscalationStructure
 import uuid
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext as _
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
 
 # Our user model
 class Complaint(models.Model):
     complaint_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
-    user_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    user_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='complaint_user')
     user_id = models.PositiveIntegerField()
     user_object = GenericForeignKey('user_type', 'user_id')
     complaint_date = models.DateTimeField(auto_now_add=True)
@@ -21,7 +21,7 @@ class Complaint(models.Model):
     complaint_title = models.CharField(max_length=100, editable=True)
 
     # Should be able to assign to multiple models such as Counsellor, ClassTeacher, HOD
-    content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE, related_name='complaint_owner')
     object_id = models.PositiveIntegerField()
     assigned_to = GenericForeignKey('content_type', 'object_id')
 
@@ -35,7 +35,7 @@ class Complaint(models.Model):
     )
 
     def __str__(self):
-        return f'Complaint by {self.user.username} at {self.complaint_time}'
+        return f'Complaint by {self.user_object.user.username} at {self.complaint_time}'
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
