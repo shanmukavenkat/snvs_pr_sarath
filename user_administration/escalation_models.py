1f46ac8abfe702a385b6ab6f14462f5363b60dca
BhargavBunty <bhargavbunty@059gmail.com>
2024-03-19 00:15:37 +0530

    Created a hierarchial model that defines escalation flow of complaint

--- user_administration/escalation_models.py
+++ user_administration/escalation_models.py
@@ -5,6 +5,9 @@ from django.core.validators import MinLengthValidator
 class Role(models.Model):
     role = models.CharField(max_length=20, validators=[MinLengthValidator(4)], primary_key=True)
 
+    def __str__(self):
+        return f'{self.role}'
+
 
 class EscalationStructure(models.Model):
     class Meta:
@@ -13,6 +16,9 @@ class EscalationStructure(models.Model):
             # The role must be unique w.r.t id
             models.UniqueConstraint(fields=['id', 'role'], name='unique_id_role')
         ]
-
     id = models.IntegerField(primary_key=True)
     role = models.ForeignKey(to=Role, to_field='role', on_delete=models.CASCADE)
+    student_relation_identifier = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.role_id}'
