48a6a309ce9ada9d75481e860448862f22021b1a
BhargavBunty <bhargavbunty@059gmail.com>
2024-03-19 00:08:02 +0530

    Added urls for view_complaint and escalate complaint

--- complaint_administration/urls.py
+++ complaint_administration/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from complaint_registration import views
+from complaint_administration import views
 
 urlpatterns = [
     path('register_complaint/', views.register_complaint),
     path('view_complaints/', views.view_complaints),
-    path('search_complaints/', views.search)
+    path('search_complaints/', views.search),
+    path('view_complaint/<slug:complaint_id>/', views.view_complaint, name='view_complaint'),
+    path('escalate_complaint/<slug:complaint_id>/', views.escalate_complaint, name='escalate_complaint')
 ]
