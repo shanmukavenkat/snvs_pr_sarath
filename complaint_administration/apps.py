cd743fd8748eae729e125196454ae48bdc041035
BhargavBunty <bhargavbunty@059gmail.com>
2024-03-19 00:17:53 +0530

    Minor changes and refactoring

--- complaint_administration/apps.py
+++ complaint_administration/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ComplaintRegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'complaint_registration'
+    name = 'complaint_administration'
